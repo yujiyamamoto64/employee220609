2d492105d4a87cf24110f7e54ee7ca6735d8d25c
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-06-09 14:12:55 -0300

    first commit

---
+++ src/main/java/com/yujiyamamoto64/employee220609/Employee220609Application.java
@@ -0,0 +1,13 @@
+package com.yujiyamamoto64.employee220609;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Employee220609Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Employee220609Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/yujiyamamoto64/employee220609/Employee220609ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yujiyamamoto64.employee220609;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Employee220609ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
